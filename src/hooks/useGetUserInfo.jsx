02aec72d1fd62ba2b605bd7399b063a08b35d33d
Kiran Shrinivaas S <85959289+KiranShrinivaas8367@users.noreply.github.com>
2026-01-07 11:26:56 +0530

    hook-update

--- src/hooks/useGetUserInfo.jsx
+++ src/hooks/useGetUserInfo.jsx
@@ -1,4 +1,8 @@
 export const useGetUserInfo = () => {
+    // console.log(localStorage.getItem("auth"));
+    if(!localStorage.getItem("auth"))
+        return {userID:null, name:null, profilePhoto:null, isAuth:false};
+    
     const {userID, name, profilePhoto, isAuth} = JSON.parse(
         localStorage.getItem("auth"));
     return {userID,name,profilePhoto, isAuth}
