57e6ca19dbc0be0c9d77fcbee3c0aba9e3457714
Kiran Shrinivaas S <85959289+KiranShrinivaas8367@users.noreply.github.com>
2026-01-19 23:23:41 +0530

    lazy-loading-added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Auth } from './pages/auth/index'
 import './App.css'
@@ -9,10 +10,12 @@ function App() {
   return (
     <div className='App'>
       <Router>
+        <Suspense fallback={<div className="loading">Loading...</div>}>
         <Routes>
           <Route path='/' exact element={<Auth/>}/>
           <Route path='/expense-tracker' element={<ExpenseTracker />}/>
         </Routes>
+        </Suspense>
       </Router>
     </div>
   )
